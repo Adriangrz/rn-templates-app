ac0997462ed3ef027deaa3198a8afd8291eb334e
Adriangrz <adriang1412g@gmail.com>
2023-01-23 10:14:40 +0100

    refactor: add type to createTheming in ThemeProvider

--- providers/ThemeProvider.ts
+++ providers/ThemeProvider.ts
@@ -1,11 +1,7 @@
-import {
-  createTheming,
-  type ThemingType,
-} from "@callstack/react-theme-provider";
+import { createTheming } from "@callstack/react-theme-provider";
 import theme from "../theme";
 import { ThemeType } from "../types/ThemeType";
 
-const { ThemeProvider, withTheme, useTheme }: ThemingType<ThemeType> =
-  createTheming(theme);
+const { ThemeProvider, withTheme, useTheme } = createTheming<ThemeType>(theme);
 
 export { ThemeProvider, withTheme, useTheme };
